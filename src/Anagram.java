a90249069b9b671472f88540c9fa8c6af5bdb946
Ataklete <65136939+Ataklete@users.noreply.github.com>
2025-09-20 14:04:08 -0500

    LongestCommonPrefixNumber.class

---
+++ src/Anagram.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        List<String> str = List.of("ate", "eat", "test", "cooc", "oocc");
+        //First way
+        System.out.println(getAnagramOne(str));
+        //Second way
+        System.out.println(getAnagramTwo(str));
+    }
+
+    private static List<String> getAnagramOne(List<String> str) {
+        return str.stream().collect(Collectors.groupingBy(word -> {;
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            return new String(chars);
+        })).values().stream()
+                .filter(group -> group.size() > 1)
+                .flatMap(Collection::stream)
+                .toList();
+//                .forEach(System.out::println);
+    }
+
+    private static List<String> getAnagramTwo(List<String> str) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : str) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(word);
+//            map.merge(key, new ArrayList<>(List.of(word)), (oldList, newList) -> {
+//                oldList.addAll(newList);
+//                return oldList;
+//            });
+        }
+        return map.values().stream()
+                .filter(group -> group.size() > 1)
+                .flatMap(Collection::stream)
+                .toList();
+    }
+}
